ac4a4805240aa227a92ad8c000da84d049bcf5b2
garygao <garygao@futunn.com>
2026-01-07 17:43:26 +0800

    chore: support publish to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 5173 },
+  base: '/minesweeper-resolver/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  },
 });
